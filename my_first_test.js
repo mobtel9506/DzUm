c8833e9044d9331d61650b301a3dd01983a1191f
mobtel9506 <95219024+mobtel9506@users.noreply.github.com>
2022-02-24 16:26:18 +0600

    Add files via upload
    Lägger upp mitt test

---
+++ my_first_test.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+describe('Testing samething on Google', () => {
+    // komment
+    it ('I can serch samething', () => {
+        cy.visit('https://google.com');
+        cy.get('input [input id="zipcode"]').type('23235').type('{enter}')
+    });
+});
