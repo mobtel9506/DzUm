0fadfa92c6dfb1f1d5f10b983d6812a6b9a11c84
mobtel9506 <95219024+mobtel9506@users.noreply.github.com>
2022-02-28 13:52:57 +0600

    Update my_first_test.js

--- my_first_test.js
+++ my_first_test.js
@@ -3,6 +3,6 @@ describe('Testing samething on Google', () => {
     // komment
     it ('I can serch samething', () => {
         cy.visit('https://google.com');
-        cy.get('input [input id="zipcode"]').type('23235').type('{enter}')
+        // cy.get('input [input id="zipcode"]').type('23235').type('{enter}')
     });
 });
